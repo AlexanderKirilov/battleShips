3ba98ce5676c6389d057d3a0fb5a182b755457ac
Alexander Kirilov <alexKirilov97@gmail.com>
2016-08-21 14:51:24 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+/*
+	GRID
+	-matrix[][];
+
+ */
+
+var ships = function(){
+
+}
+
+var player = function(spec){
+	var playerObj = {};
+	
+	var ammo = (spec && spec.ammo) || 20; // handle defaults
+	var hits = 0;
+	var misses = 0;
+
+	playerObj.prototype.shoot = function(x,y){
+
+	};
+
+	return playerObj 
+}
+
+var game = function (){
+	var player = player({ammo:25});
+
+	var grid = 
+
+	return {
+
+		init: function(){
+			pl
+		}
+
+	}
+}
